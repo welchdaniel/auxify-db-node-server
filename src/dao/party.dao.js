61054fefe4cd601f134fc8c58b7e60fa4387be01
welchdaniel <welch.da@husky.neu.edu>
2019-11-08 23:55:48 -0500

    dao calls asynchronous where possible

--- src/dao/party.dao.js
+++ src/dao/party.dao.js
@@ -22,30 +22,50 @@ deleteParty = (id) => {
 }
 
 addUserToParty = async(partyId, userId) => {
-	await UserModel.updateOne({_id: userId}, {$set: {currentPartyId: partyId, currentRole: 'LISTENER'}});
-	return PartyModel.updateOne(
+	const userUpdate = await UserModel.updateOne({_id: userId}, {$set: {currentPartyId: partyId, currentRole: 'LISTENER'}});
+	const partyUpdate = PartyModel.updateOne(
 		{_id: partyId},
 		{$addToSet: {memberIds: userId}},
 		{upsert:false}
 	);
+	Promise.all([userUpdate, partyUpdate])
+		.then((response) => {
+			return response;
+	});
 }
 
 removeUserFromParty = async(partyId, userId) => {
-	await UserModel.updateOne({_id: userId}, {$set: {currentPartyId: null, currentRole: 'BROWSER'}});
-	return PartyModel.updateOne(
+	const userUpdate = UserModel.updateOne({_id: userId}, {$set: {currentPartyId: null, currentRole: 'BROWSER'}});
+	const partyUpdate = PartyModel.updateOne(
 		{_id: partyId},
 		{$pull: {memberIds: userId}},
 		{upsert: false}
 	);
+	Promise.all([userUpdate, partyUpdate])
+		.then((response) => {
+			return response;
+	});
 }
 
 setPartyLeader = async(partyId, userId) => {
-	await UserModel.updateOne({_id: userId}, {$set: {currentPartyId: partyId, currentRole: 'DJ'}});
-	return PartyModel.updateOne(
+	const party = await PartyModel.findById(partyId);
+	const currentPartyLeaderId = party.partyLeaderId;
+	if (currentPartyLeaderId === userId) {
+		throw "Specified user is already the DJ";
+	}
+	if (currentPartyLeaderId) {
+		await UserModel.updateOne({_id: currentPartyLeaderId}, {$set: {currentRole: 'LISTENER'}});
+	}
+	const userUpdate = UserModel.updateOne({_id: userId}, {$set: {currentPartyId: partyId, currentRole: 'DJ'}});
+	const partyUpdate = PartyModel.updateOne(
 		{_id: partyId},
 		{$addToSet: {memberIds: userId}, $set: {partyLeaderId: userId}},
 		{upsert: false}
 	);
+	Promise.all([userUpdate, partyUpdate])
+		.then((response) => {
+			return response;
+	});
 }
 
 module.exports = {
